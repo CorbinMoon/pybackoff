554f10131c202c46da9379892be6aceae6ac631a
CorbinMoon <clangmoon@gmail.com>
2021-12-05 11:49:33 -0500

    refactored install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # install cmake
-if ! [ "$(dpkg -l | grep cmake)" -eq 0 ] ; then
-  sudo apt install cmake
-fi
-
-#install boost-python
-if ! [ "$(dpkg -l | grep boost)" -eq 0 ] ; then 
-  sudo apt install libboost-all-dev
-fi
+sudo apt install cmake
+# install boost libraries
+sudo apt install libboost-all-dev
